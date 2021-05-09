3db9ebcea13015ba7e54ada03944663f0a19ba65
Jakowlew <jakowlew@pm.me>
2021-05-10 02:46:26 +0300

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+﻿//------------------------------------------
+// Author : 
+// Sakhil Kuliev
+// Joshua Jakowlew
+//------------------------------------------
+// Standard Library Including
+   #include <iostream>
+
+// Internal including
+
+//------------------------------------------
+// External Library including
+ 
+//------------------------------------------
+// Preprocessor Directives
+
+//------------------------------------------
+// Star of program
+int main() {
+    std::cout << "Hello Cmake!" << std::endl;
+}
